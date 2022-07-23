876cf046c6e2eb25585016be65d0c098b6a8acc7
rockstararyaman <93260229+rockstararyaman@users.noreply.github.com>
2022-07-23 15:38:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-// https://teachablemachine.withgoogle.com/models/upJmuyWLm/
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/upJmuyWLm/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
